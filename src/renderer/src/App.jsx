d204ad8470046e5cd95410b67fdf7d46d257c9b0
Allan da Silva <allanweb019@gmail.com>
2024-05-16 22:36:37 -0300

    Login

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
--- src/renderer/src/components/Header.jsx
+++ src/renderer/src/components/Header.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return <>
-    <Link to="/">Home</Link>
-    <Link to="/register">Register</Link>
-    <Link to="/login">Login</Link>
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
     </>;
 }
 
